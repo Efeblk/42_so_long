67c0b0716d5c15e3aa9db7b76dd561bf29cab475
ismet efe balık <efebalikofc@hotmail.com>
2023-08-10 03:39:21 +0300

    mapcheck update

--- mapread.c
+++ mapread.c
@@ -17,30 +17,58 @@ static int charvalid(char c)
 
 static void checkline(char *line)
 {
+    int last;
+
+    last = ft_strlen1(line) - 1;
+    if (line[0] != '1' || line[last] != '1')
+        exitor();
     while (*line && charvalid(*line))
-    {
         line++;
+}
+
+static void isallone(char *mapline)
+{
+    while (*mapline)
+    {
+        if (*mapline != '1' && *mapline != '\n')
+            exitor();
+        mapline++;
+    }  
+}
+
+static void firstlinecheck(int firstlinebool, char *mapline)
+{
+    static int mapwidth;
+    int tmpwidth;
+
+    if (firstlinebool)
+    {
+        mapwidth = ft_strlen1(mapline);
+        isallone(mapline);
     }
+    tmpwidth = ft_strlen1(mapline);
+    if (mapwidth != tmpwidth)
+        exitor();
 }
 
 void readmap(char *mapname)
 {
     int fdmap;
     char *mapline;
-    int mapwidth;
+    int firstlinebool;
 
+    firstlinebool = 1;
     mapline = "tmp";
     fdmap = open(mapname, O_RDONLY);
     while (mapline != NULL)
     {
         mapline = get_next_line(fdmap);
         if (mapline == NULL)
-        {
             break;
-        }
-        checkline(mapline);
-        mapwidth = ft_strlen1(mapline);
-        printf("\n%i", mapwidth);
+
+        firstlinecheck(firstlinebool, mapline);
+        firstlinebool = 0;
+        checkline(mapline);     
     }
 }
 
